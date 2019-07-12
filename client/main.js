b7fd3bea88a6c2a7c142dd5683551acb93ae7f39
yashbhati283 <yashbhati283@gmail.com>
2019-07-13 03:32:52 +0530

    Update main.js

--- client/main.js
+++ client/main.js
@@ -4,7 +4,14 @@ this.Documents = new Mongo.Collection("documents");
 
 import './main.html';
 
-
+Meteor.setInterval(function(){
+   Session.set("date",new Date()) 
+},1000);
+Template.current_date.helpers({
+   date:function(){
+       return Session.get("date");
+   }
+});
 
 Template.editor.helpers({
     docid:function(){
