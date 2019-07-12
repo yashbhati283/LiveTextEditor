a211a234ec4ce506c1320ba227bd0899b1681955
yashbhati283 <yashbhati283@gmail.com>
2019-07-13 03:17:26 +0530

    Add files via upload

---
+++ client/main.js
@@ -0,0 +1,24 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+this.Documents = new Mongo.Collection("documents");
+
+import './main.html';
+
+
+
+Meteor.setInterval(function(){
+   Session.set("date",new Date()) 
+},1000);
+Template.current_date.helpers({
+   date:function(){
+       return Session.get("date");
+   }
+});
+
+Template.editor.helpers({
+    docid:function(){
+        var doc=Documents.findOne();
+        if(doc)return doc._id;
+        else return undefined;
+    }
+});
